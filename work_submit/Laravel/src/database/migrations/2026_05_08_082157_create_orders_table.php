37f5f19754ff603d91bf935e06776d687a6c55f6
M_nakamura <nakamura_miho@care-con.co.jp>
2026-05-08 18:31:02 +0900

    0508_現状提出

--- work_submit/Laravel/src/database/migrations/2026_05_08_082157_create_orders_table.php
+++ work_submit/Laravel/src/database/migrations/2026_05_08_082157_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password_hash');
-            $table->timestamps();
-            $table->integer('role');
-            $table->softDeletes();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('total_price');
             $table->integer('status');
+            $table->timestamps();
+            $table->integer('tax');
+            $table->string('pay');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 }
